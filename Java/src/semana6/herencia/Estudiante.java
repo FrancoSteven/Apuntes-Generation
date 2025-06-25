4a5a10e4475ae91bce4eaf395b28d10b76f477ad
Guido Perez <gperzal@gmail.com>
2025-06-25 19:39:15 -0400

    feat(herencia): ✨🧬 added basic OOP inheritance example 👩‍🎓👨‍🎓 with student classe

---
+++ Java/src/semana6/herencia/Estudiante.java
@@ -0,0 +1,21 @@
+package semana6.herencia;
+
+public class Estudiante {
+
+    protected String nombre;
+    protected int edad;
+
+    public Estudiante(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void estudiar() {
+        System.out.println("Esta estudiando");
+    }
+
+    public void getNombre() {
+        System.out.println("Nombre: " + nombre);
+    }
+
+}
---
+++ Java/src/semana6/herencia/EstudianteSecundaria.java
@@ -0,0 +1,18 @@
+package semana6.herencia;
+
+public class EstudianteSecundaria extends Estudiante{
+
+   private String especialidad;
+
+    public EstudianteSecundaria(String nombre, int edad, String especialidad) {
+        super(nombre, edad);
+        this.especialidad = especialidad;
+    }
+
+    // Anotacion: Es para sobre escribir metodos
+    @Override
+    public void estudiar() {
+        System.out.println("Estudiante " + nombre + " esta estudiando en " + especialidad);
+    }
+
+}
